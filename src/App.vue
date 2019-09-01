9d56da31423231dee31ee4f02d47c7dc752d1b6e
unknown <aman@ratiocinativesolutions.com>
2019-09-02 01:37:32 +0530

    Intial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+      <message>hello</message>
+  </div>
+</template>
+
+<script>
+import Message from './components/Message.vue';
+export default {
+  name: 'app',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box">
+        <p>
+            <slot></slot>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+    .box {
+        width: 20%;
+        margin: 0 auto;
+    }
+</style>
